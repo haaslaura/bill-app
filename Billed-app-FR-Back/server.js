dd127f9c04d8b404e70d5794d0ebe55943702f50
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-07-19 11:15:31 +0200

    add backend folder

---
+++ Billed-app-FR-Back/server.js
@@ -0,0 +1,8 @@
+const app = require('./app');
+
+// app.listen(5678, () => {
+app.listen(5500, () => {
+  // eslint-disable-next-line no-console
+  // console.log('Example app listening on port 5678!');
+  console.log('Example app listening on port 5500!');
+});
