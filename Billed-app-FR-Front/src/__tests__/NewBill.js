63cd3e3b222ba3ca15019e2eda62d462be6e9ab7
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-08-02 10:44:31 +0200

    test NewBill post - work in progress

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,70 +2,62 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen, waitFor } from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
 
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import NewBill from "../containers/NewBill.js";
+import NewBillUI from "../views/NewBillUI.js";
+import BillsUI from "../views/BillsUI.js";
 
-import { ROUTES_PATH, ROUTES } from "../constants/routes.js"
-import { localStorageMock } from "../__mocks__/localStorage.js"
-import router from "../app/Router.js"
-import store from "../__mocks__/store.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router";
 
-  // 53 - 71 Envoi du formulaire
-  // Vérifier si les données sont bien envoyées, avec la méthode post
+jest.mock("../app/Store", () => mockStore);
+
 
 describe("Given I am connected as an employee", () => {
+  
   describe("When I am on NewBill Page", () => {
-
-    test("Then send icon in vertical layout should be highlighted", async () => {
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
+    
+    beforeEach(() => {
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem("user", JSON.stringify({
+        type: "Employee"
       }));
+    });
+    
+    test("Then send icon in vertical layout should be highlighted", () => {
       
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.NewBill);
-      await waitFor(() => screen.getByTestId('icon-mail'));
       const mailIcon = screen.getByTestId('icon-mail');
-
+      
       // Check if the icon is present
       expect(mailIcon.className).toBe('active-icon');
     });
-
-    test("Then the submission form should be displayed", async () => {
-      document.body.innerHTML = NewBillUI();
+    
+    test("Then the submission form should be displayed", () => {
       
-      const title = await screen.findByText("Envoyer une note de frais");
-      expect(title).toBeTruthy();
-
-      const newBillForm = screen.getByTestId("form-new-bill");
-      expect(newBillForm).toBeTruthy();
+      document.body.innerHTML = NewBillUI();
+      expect(screen.findByText("Envoyer une note de frais")).toBeTruthy();
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
     });
   })
 });
 
 describe("Given I am connected as an employee and I am on the NewBill Page", () => {
-
-  beforeEach(() => {
-    document.body.innerHTML = NewBillUI();
-    Object.defineProperty(window, 'localStorage', {
-      value: {
-        getItem: jest.fn(() => JSON.stringify({ type: 'Employee', email: "a@a" })),
-        setItem: jest.fn(),
-      },
-      writable: true,
-    });
-  });
   
   describe("When I do not fill fields and I click on the sending button", () => {
-    test("Then It should renders the NewBill Page", async() => {
 
+    test("Then It should renders the NewBill Page", () => {
+      
       const newBillForm = screen.getByTestId("form-new-bill");
-
+      
       // Simulate empty fields
       const inputExpenseName = screen.getByTestId("expense-name");
       expect(inputExpenseName.value).toBe("");
@@ -81,19 +73,20 @@ describe("Given I am connected as an employee and I am on the NewBill Page", () 
       expect(inputCommentary.value).toBe("");
       const inputFile = screen.getByTestId("file");
       expect(inputFile.value).toBe("");
-
+      
       // Simulate sending the form
       const handleSubmit = jest.fn((e) => e.preventDefault());
       newBillForm.addEventListener("submit", handleSubmit);
       fireEvent.submit(newBillForm);
-
-      await waitFor(() => expect(screen.getByTestId("form-new-bill")).toBeTruthy());
+      
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
     });
   });
   
   describe("When I do fill fields in correct format, exept the file, and I click on the sending button", () => {
-    test("Then It should renders the NewBill Page without sending data", async () => {
-      
+    
+    test("Then It should renders the NewBill Page", () => {
+
       // Simulate data
       const newBillData = {
         type: "Restaurants et bars",
@@ -103,15 +96,14 @@ describe("Given I am connected as an employee and I am on the NewBill Page", () 
         amount: 110,
         pct: 10,
         vat: "9",
-        commentAdmin: "",
         fileName: "invalid-file.txt"
       };
-
+      
       // Simulate changes in field content
       const inputExpenseType = screen.getByTestId("expense-type");
       fireEvent.change(inputExpenseType, { target: { value: newBillData.type }});
       expect(inputExpenseType.value).toBe(newBillData.type);
-
+      
       const inputExpenseName = screen.getByTestId("expense-name");
       fireEvent.change(inputExpenseName, { target: { value: newBillData.name }});
       expect(inputExpenseName.value).toBe(newBillData.name);
@@ -136,59 +128,72 @@ describe("Given I am connected as an employee and I am on the NewBill Page", () 
       fireEvent.change(inputCommentary, { target: { value: newBillData.commentary }});
       expect(inputCommentary.value).toBe(newBillData.commentary);
       
-      // Simulate an erroneous file
       const inputFile = screen.getByTestId("file");
-      fireEvent.change(inputFile, {
-        target: {
-          files: [new File(["paslebonformat"], "invalid-file.txt", {type: "text/plain"})],
-        }
-      });
-
+      
+      
       // Simulate the naviation to test it
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname });
       };
-
-      const store = jest.fn();
       
       const sending = new NewBill({
         document,
         onNavigate,
-        store,
+        store: null,
         localStorage: window.localStorage
       });
-
-      const newBillForm = screen.getByTestId("form-new-bill");
       
-      // Simulate file change
-      const handleChangeFile = jest.fn(sending.handleChangeFile);
-      sending.updateBill = jest.fn().mockResolvedValue({});
+      // Simulate handleChangeFile() function
+      const handleChangeFile = jest.fn((e) => sending.handleChangeFile(e));
       inputFile.addEventListener("change", handleChangeFile);
       fireEvent.change(inputFile, {
         target: {
-          files: [new File(["paslebonformat"], newBillData.fileName, { type: "text/plain" })]
+          files: [new File(["paslebonformat"], newBillData.fileName, {type: "text/plain"})],
         }
       });
-      await waitFor(() => expect(handleChangeFile).toHaveBeenCalled());
-
+      expect(handleChangeFile).toHaveBeenCalled();
+      
       // Simulate sending the form
+      const newBillForm = screen.getByTestId("form-new-bill");
       const handleSubmit = jest.fn((e) => e.preventDefault());
+      
       newBillForm.addEventListener("submit", handleSubmit);
       fireEvent.submit(newBillForm); 
-      
-      // Check that data has not been sent
-      await waitFor(() => expect(handleSubmit).toHaveBeenCalled());
-      expect(sending.updateBill).not.toHaveBeenCalled();
-
-      // Check the form is still displayed
       expect(screen.getByTestId("form-new-bill")).toBeTruthy();
     });
   });
-
-  // Ajouter un test d'intégration POST new bill
+  
   describe("When I do fill fields in correct format and I click on the sending button", () => {
+    
+    beforeEach(() => {
+      // Mock localStorage
+      jest.spyOn(mockStore, "bills");
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      );
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "a@a"
+      }));
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.appendChild(root);
+      router();
+    });
+
+
+    // Vérifier l'intégration avec la méthode POST
     test("Then the data is sent correctly", async () => {
 
+      // Simulate the navigation to test it
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      window.onNavigate(ROUTES_PATH.NewBill);
+
+      
       // Simulate data
       const newBillData = {
         type: "Restaurants et bars",
@@ -198,29 +203,23 @@ describe("Given I am connected as an employee and I am on the NewBill Page", () 
         amount: 110,
         pct: 10,
         vat: "9",
-        commentAdmin: "",
-        fileName: "scan-ticket-de-caisse-avec-tva.jpg"
+        fileName: "scan-ticket-de-caisse.jpg"
       };
 
-      // const newBillData = {
-      //   id: "47qBXb6fIm2zOKqLzZri",
-      //   vat: "9",
-      //   status: "pending",
-      //   type: "Restaurants et bars",
-      //   commentary: "invitation du client untel à déjeuner",
-      //   name: "Invitation client",
-      //   fileName: "scan-ticket-de-caisse-avec-tva.jpg",
-      //   date: "2004-04-04",
-      //   amount: 110,
-      //   commentAdmin: "",
-      //   email: "a@a",
-      //   pct: 10
-      // };
+      const sending = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage
+      });
 
+      document.body.innerHTML = NewBillUI();
+      
+      // Simulate changes in field content
       const inputExpenseType = screen.getByTestId("expense-type");
       fireEvent.change(inputExpenseType, { target: { value: newBillData.type}});
       expect(inputExpenseType.value).toBe(newBillData.type);
-
+      
       const inputExpenseName = screen.getByTestId("expense-name");
       fireEvent.change(inputExpenseName, { target: { value: newBillData.name}});
       expect(inputExpenseName.value).toBe(newBillData.name);
@@ -246,66 +245,87 @@ describe("Given I am connected as an employee and I am on the NewBill Page", () 
       expect(inputCommentary.value).toBe(newBillData.commentary);
       
       const inputFile = screen.getByTestId("file");
-      fireEvent.change(inputFile, {
-        target: {
-          files: [new File(["bonformat"], newBillData.fileName, { type: "image/jpg" })]
-        }
-      });
-      expect(inputFile.value).toBe(newBillData.fileName);
-
-      // Simulate the naviation to test it
-      const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname });
-      };
-
-      const store = jest.fn();
       
-      const sending = new NewBill({
-        document,
-        onNavigate,
-        store,
-        localStorage: window.localStorage
-      });
+      
 
-      const newBillForm = screen.getByTestId("form-new-bill");
-
-      // Simulate file change
-      const handleChangeFile = jest.fn(sending.handleChangeFile);
-      sending.updateBill = jest.fn().mockResolvedValue({});
+      
+      // Simulate handleChangeFile() function
+      const handleChangeFile = jest.fn((e) => sending.handleChangeFile(e));
       inputFile.addEventListener("change", handleChangeFile);
       fireEvent.change(inputFile, {
         target: {
-          files: [new File(["bonformat"], newBillData.fileName, { type: "image/jpg" })]
+          files: [new File(["bonformat"], newBillData.fileName, {type: "image/jpg"})],
         }
       });
-      await waitFor(() => expect(handleChangeFile).toHaveBeenCalled());
+      expect(handleChangeFile).toHaveBeenCalled();
+      
+
+      // Mock la méthode create de bills pour simuler l'envoi des données au serveur
+      // const mockCreate = jest.fn(mockStore.bills().create);
+      // mockStore.bills = jest.fn(() => ({
+      //   create: mockCreate,
+      // }));
+      mockStore.bills.mockImplementation(() => {
+        return {
+          create: () => {
+            return Promise.resolve({
+              fileUrl: `${sending.filePath}`,
+              key: "1234",
+            });
+          },
+          update: () => {
+            return Promise.resolve({
+              type: "Restaurants et bars",
+              name: "Invitation client",
+              commentary: "invitation du client untel à déjeuner",
+              date: "2004-04-04",
+              amount: 110,
+              pct: 10,
+              vat: "9",
+              fileName: "scan-ticket-de-caisse.jpg"
+            });
+          },
+        };
+      });
+
 
       // Simulate sending the form
-      const handleSubmit = jest.fn((e) => e.preventDefault());
-      // notes : on a besoin d'une fonction mais submit gère déjà l'envoi
+      const newBillForm = screen.getByTestId("form-new-bill");
+
+      const handleSubmit = jest.fn((e) => sending.handleSubmit(e));
+      // sending.updateBill = jest.fn().mockResolvedValue({});
+
       newBillForm.addEventListener("submit", handleSubmit);
-      fireEvent.submit(newBillForm); 
+      fireEvent.submit(newBillForm);
+      expect(handleSubmit).toHaveBeenCalled();
 
-      // Vérifier ce qui est appelé
-      await waitFor(() => expect(handleSubmit).toHaveBeenCalled());
+      // Vérifier que updateBill est bien appelé
+      // expect(sending.updateBill).toHaveBeenCalled();
 
-      expect(sending.updateBill).toHaveBeenCalledWith({
-        email: "a@a",
-        type: newBillData.type,
-        name: newBillData.name,
-        amount: newBillData.amount,
-        date: newBillData.date,
-        vat: newBillData.vat,
-        pct: newBillData.pct,
-        commentary: newBillData.commentary,
-        filePath: undefined, // pas sur !!!!
-        fileName: newBillData.fileName, // pas sur !!!!
-        status: "pending",
-      });
+      // Attendre que la méthode create soit appelée
+      // await waitFor(() => expect(mockCreate).toHaveBeenCalledWith(expect.objectContaining({
+      //   email: "a@a",
+      //   type: newBillData.type,
+      //   name: newBillData.name,
+      //   amount: newBillData.amount,
+      //   date: newBillData.date,
+      //   vat: newBillData.vat,
+      //   pct: newBillData.pct,
+      //   commentary: newBillData.commentary,
+      //   filePath: expect.any(String),
+      //   fileName: newBillData.fileName,
+      //   status: "pending",
+      // })));
+      
+      expect(mockStore.bills).toHaveBeenCalled();
+      
+      // await waitFor(() => screen.getByText("Mes notes de frais"));
+      // expect(screen.getByText("Mes notes de frais")).toBeTruthy();
     });
+    
+    // test("Then it should renders the Bills page", () => {
 
-    // test("Then it should renders the Bills page", async () => {
-    //   await waitFor (() => expect(screen.getByText("Mes notes de frais")).toBeTruthy());
     // });
   });
+
 });
